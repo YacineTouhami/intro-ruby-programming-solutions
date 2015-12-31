0c7eebd5af9b1fea5473ce20429642bae3503d8a
Yacine Touhami <spino0505@gmail.com>
2015-12-31 14:49:09 +0100

    Solutions for chapter Methods

---
+++ Methods/ex_3.rb
@@ -0,0 +1,4 @@
+def mutiply(a,b)
+  a * b
+end
+puts mutiply(2,3)
---
+++ Methods/ex_4.rb
@@ -0,0 +1,7 @@
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yiiiiiiip") # It will not print anything to the screen
